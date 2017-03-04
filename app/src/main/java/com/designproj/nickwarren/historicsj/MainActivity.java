18a26f2190cb41d8936f0ea9b58466ddf362a669
ncwarren <n.warren@mun.ca>
2017-03-03 23:40:13 -0330

    test commit

--- app/src/main/java/com/designproj/nickwarren/historicsj/MainActivity.java
+++ app/src/main/java/com/designproj/nickwarren/historicsj/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
+    //testaddition
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
